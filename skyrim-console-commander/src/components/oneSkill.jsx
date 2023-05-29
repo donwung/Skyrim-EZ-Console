cbfded4694e7b4b7cc0a2edc53625d2dd41c49e0
donwung <113479299+donwung@users.noreply.github.com>
2023-05-29 13:32:26 -0700

    created component to lessen weight on app.jsx

---
+++ skyrim-console-commander/src/components/oneSkill.jsx
@@ -0,0 +1,24 @@
+const OneSkill = (props) => {
+    const skill = props.skill
+    const handleOnCheckedSkill = props.handleOnCheckedSkill
+    const handleOnSkillChange = props.handleOnSkillChange
+
+    return (
+        <div style={{ color: skill.selected ? "green" : "grey" }}>
+            <input
+                type="checkbox"
+                name={skill.name.replace(/\s/g, '').toLowerCase()}
+                onChange={(e) => { handleOnCheckedSkill(e.target.name) }}
+            ></input>
+            <label>{skill.name}</label>
+            <input
+                type="number"
+                name={skill.name.replace(/\s/g, '').toLowerCase()}
+                onChange={(e) => handleOnSkillChange(e)}
+                value={skill.selected ? skill.level : 0}
+            ></input>
+        </div>
+    )
+}
+
+export default OneSkill;
