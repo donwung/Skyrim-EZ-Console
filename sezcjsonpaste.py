15b927eff22e482406a208570ea8d76e0c46e0ff
donwung <113479299+donwung@users.noreply.github.com>
2023-05-29 19:07:03 -0700

    changed this to further resemble json

--- sezcjsonpaste.py
+++ sezcjsonpaste.py
@@ -12,131 +12,137 @@ html_doc = """
 <th class="headerSort" tabindex="0" role="columnheader button" title="Sort ascending">Perk Req.</th>
 </tr></thead><tbody>
 <tr>
-<td><i><span id="Novice_Conjuration"></span>Novice Conjuration</i></td>
+<td><i><span id="Novice_Destruction"></span>Novice Destruction</i></td>
 <td></td>
-<td>Cast Novice level Conjuration spells for half magicka.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0f2ca7</span></span></span></td>
+<td>Cast Novice level Destruction spells for half magicka.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0f2ca8</span></span></span></td>
 <td></td>
 <td></td>
 </tr>
 <tr>
-<td><i><span id="Apprentice_Conjuration"></span>Apprentice Conjuration</i></td>
+<td><i><span id="Apprentice_Destruction"></span>Apprentice Destruction</i></td>
 <td></td>
-<td>Cast Apprentice level Conjuration spells for half magicka.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0c44bb</span></span></span></td>
-<td>25 Conjuration</td>
-<td>Novice Conjuration</td>
+<td>Cast Apprentice level Destruction spells for half magicka.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0c44bf</span></span></span></td>
+<td>25 Destruction</td>
+<td>Novice Destruction</td>
 </tr>
 <tr>
-<td><i><span id="Adept_Conjuration"></span>Adept Conjuration</i></td>
+<td><i><span id="Adept_Destruction"></span>Adept Destruction</i></td>
 <td></td>
-<td>Cast Adept level Conjuration spells for half magicka.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0c44bc</span></span></span></td>
-<td>50 Conjuration</td>
-<td>Apprentice Conjuration</td>
+<td>Cast Adept level Destruction spells for half magicka.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0c44c0</span></span></span></td>
+<td>50 Destruction</td>
+<td>Apprentice Destruction</td>
 </tr>
 <tr>
-<td><i><span id="Expert_Conjuration"></span>Expert Conjuration</i></td>
+<td><i><span id="Expert_Destruction"></span>Expert Destruction</i></td>
 <td></td>
-<td>Cast Expert level Conjuration spells for half magicka.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0c44bd</span></span></span></td>
-<td>75 Conjuration</td>
-<td>Adept Conjuration</td>
+<td>Cast Expert level Destruction spells for half magicka.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0c44c1</span></span></span></td>
+<td>75 Destruction</td>
+<td>Adept Destruction</td>
 </tr>
 <tr>
-<td><i><span id="Master_Conjuration"></span>Master Conjuration</i></td>
+<td><i><span id="Master_Destruction"></span>Master Destruction</i></td>
 <td></td>
-<td>Cast Master level Conjuration spells for half magicka.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0c44be</span></span></span></td>
-<td>100 Conjuration</td>
-<td>Expert Conjuration</td>
+<td>Cast Master level Destruction spells for half magicka.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0c44c2</span></span></span></td>
+<td>100 Destruction</td>
+<td>Expert Destruction</td>
 </tr>
 <tr>
-<td><i><span id="Conjuration_Dual_Casting"></span>Conjuration Dual Casting</i></td>
+<td><i><span id="Rune_Master"></span>Rune Master</i></td>
 <td></td>
-<td><a href="/wiki/Skyrim:Magic_Overview#Dual-Casting" title="Skyrim:Magic Overview">Dual casting</a> a Conjuration spell overcharges the spell, allowing it to last longer.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0153ce</span></span></span></td>
-<td>20 Conjuration</td>
-<td>Novice Conjuration</td>
+<td>Can place runes five times farther away.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">105f32</span></span></span></td>
+<td>40 Destruction</td>
+<td>Apprentice Destruction</td>
 </tr>
 <tr>
-<td><i><span id="Mystic_Binding"></span>Mystic Binding</i></td>
-<td></td>
-<td>Bound weapons do more damage.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0640b3</span></span></span></td>
-<td>20 Conjuration</td>
-<td>Novice Conjuration</td>
-</tr>
-<tr>
-<td><i><span id="Soul_Stealer"></span>Soul Stealer</i></td>
-<td></td>
-<td>Bound weapons cast Soul Trap on targets.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0d799e</span></span></span></td>
-<td>30 Conjuration</td>
-<td>Mystic Binding</td>
-</tr>
-<tr>
-<td><i><span id="Oblivion_Binding"></span>Oblivion Binding</i></td>
-<td></td>
-<td>Bound weapons will banish summoned creatures and turn raised ones.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0d799c</span></span></span></td>
-<td>50 Conjuration</td>
-<td>Soul Stealer</td>
-</tr>
-<tr>
-<td><i><span id="Necromancy"></span>Necromancy</i></td>
-<td></td>
-<td>Greater duration for reanimated undead.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0581dd</span></span></span></td>
-<td>40 Conjuration</td>
-<td>Novice Conjuration</td>
-</tr>
-<tr>
-<td><i><span id="Dark_Souls"></span>Dark Souls</i></td>
-<td></td>
-<td>Reanimated undead have 100 points more health.<sup id="cite_ref-Zombie_1-0" class="reference"><a href="#cite_note-Zombie-1">[1]</a></sup></td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0581de</span></span></span></td>
-<td>70 Conjuration</td>
-<td>Necromancy</td>
-</tr>
-<tr>
-<td rowspan="2"><i><span id="Summoner"></span>Summoner</i></td>
+<td rowspan="2"><i><span id="Augmented_Flames"></span>Augmented Flames</i></td>
 <td>1</td>
-<td>Can summon atronachs or raise undead twice as far away.<sup id="cite_ref-Dremora_2-0" class="reference"><a href="#cite_note-Dremora-2">[2]</a></sup></td>
-<td><span class="idall"><span class="idref">00<span class="idcase">105f30</span></span></span></td>
-<td>30 Conjuration</td>
-<td>Novice Conjuration</td>
+<td>Fire spells do 25% more damage.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0581e7</span></span></span></td>
+<td>30 Destruction</td>
+<td>Novice Destruction</td>
 </tr>
 <tr>
 <td>2</td>
-<td>Can summon atronachs or raise undead three times as far away.<sup id="cite_ref-Dremora_2-1" class="reference"><a href="#cite_note-Dremora-2">[2]</a></sup></td>
-<td><span class="idall"><span class="idref">00<span class="idcase">105f31</span></span></span></td>
-<td>70 Conjuration</td>
-<td></td>
+<td>Fire spells do 50% more damage.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">10fcf8</span></span></span></td>
+<td>60 Destruction</td>
+<td>Augmented Flames Level 1</td>
 </tr>
 <tr>
-<td><i><span id="Atromancy"></span>Atromancy</i></td>
+<td><i><span id="Intense_Flames"></span>Intense Flames</i></td>
 <td></td>
-<td>Double duration for conjured Atronachs.<sup id="cite_ref-Dremora_2-2" class="reference"><a href="#cite_note-Dremora-2">[2]</a></sup></td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0cb419</span></span></span></td>
-<td>40 Conjuration</td>
-<td>Summoner</td>
+<td>Fire damage causes targets to flee if their health is low (under 20%).</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0f392e</span></span></span></td>
+<td>50 Destruction</td>
+<td>Augmented Flames</td>
 </tr>
 <tr>
-<td><i><span id="Elemental_Potency"></span>Elemental Potency</i></td>
-<td></td>
-<td>Conjured Atronachs are 50% more powerful.</td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0cb41a</span></span></span></td>
-<td>80 Conjuration</td>
-<td>Atromancy</td>
+<td rowspan="2"><i><span id="Augmented_Frost"></span>Augmented Frost</i></td>
+<td>1</td>
+<td>Frost spells do 25% more damage.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0581ea</span></span></span></td>
+<td>30 Destruction</td>
+<td>Novice Destruction</td>
 </tr>
 <tr>
-<td><i><span id="Twin_Souls"></span>Twin Souls</i></td>
+<td>2</td>
+<td>Frost spells do 50% more damage.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">10fcf9</span></span></span></td>
+<td>60 Destruction</td>
+<td>Augmented Frost Level 1</td>
+</tr>
+<tr>
+<td><i><span id="Deep_Freeze"></span>Deep Freeze</i></td>
 <td></td>
-<td>You can have two atronachs or reanimated zombies.<sup id="cite_ref-Dremora_2-3" class="reference"><a href="#cite_note-Dremora-2">[2]</a></sup></td>
-<td><span class="idall"><span class="idref">00<span class="idcase">0d5f1c</span></span></span></td>
-<td>100 Conjuration</td>
-<td>Dark Souls <i>or</i> Elemental Potency</td>
+<td>Frost damage paralyzes targets if their health is low (under 20%).<sup class="reference plainlinks nourlexpansion" id="note_stability"><a href="#intnote_stability">†</a></sup></td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0f3933</span></span></span></td>
+<td>60 Destruction</td>
+<td>Augmented Frost</td>
+</tr>
+<tr>
+<td rowspan="2"><i><span id="Augmented_Shock"></span>Augmented Shock</i></td>
+<td>1</td>
+<td>Shock spells do 25% more damage.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">058200</span></span></span></td>
+<td>30 Destruction</td>
+<td>Novice Destruction</td>
+</tr>
+<tr>
+<td>2</td>
+<td>Shock spells do 50% more damage.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">10fcfa</span></span></span></td>
+<td>60 Destruction</td>
+<td>Augmented Shock Level 1</td>
+</tr>
+<tr>
+<td><i><span id="Disintegrate"></span>Disintegrate</i></td>
+<td></td>
+<td>Shock spells disintegrate targets if their health is low (under 15%).</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0f3f0e</span></span></span></td>
+<td>70 Destruction</td>
+<td>Augmented Shock</td>
+</tr>
+<tr>
+<td><i><span id="Destruction_Dual_Casting"></span>Destruction Dual Casting</i></td>
+<td></td>
+<td><a href="/wiki/Skyrim:Magic_Overview#Dual-Casting" title="Skyrim:Magic Overview">Dual casting</a> a Destruction spell overcharges the effects into an even more powerful version.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0153cf</span></span></span></td>
+<td>20 Destruction</td>
+<td>Novice Destruction</td>
+</tr>
+<tr>
+<td><i><span id="Impact"></span>Impact</i></td>
+<td></td>
+<td>Most destruction spells will stagger an opponent when dual cast.</td>
+<td><span class="idall"><span class="idref">00<span class="idcase">0153d2</span></span></span></td>
+<td>40 Destruction</td>
+<td>Destruction Dual Casting</td>
 </tr>
 </tbody><tfoot></tfoot></table>
 """
@@ -169,17 +175,15 @@ for tableRow in soup.tbody.find_all('tr'):
             if not (re.search("[0-9][0-9]\s", tableData.get_text())):
                 if (tableData.get_text() not in memo):
                     if (re.search("[.]", tableData.get_text())):
-                        print('"' + "description" + '":' +
-                              '"' + tableData.get_text() + '"')
+                        print('"' + "description" + '":' + '"' + tableData.get_text() + '",')
                     else:
                         if (not tableData.get_text().isnumeric()):
-                            if (len(tableData.get_text() + "") == 8):
-                                print('"' + "ID" + '":' + '"' +
-                                      tableData.get_text() + '"')
+                            if(len(tableData.get_text() + "") == 8):
+                                print('"' + "ID" + '":' +'"' + tableData.get_text() + '"},')
                             else:
-                                print('"' + tableData.get_text() + '"')
+                                print('"' + tableData.get_text() + '":{')
                             memo.append(tableData.get_text())
                         else:
-                            print('"' + tableData.get_text() + '":')
+                            print('"' + tableData.get_text() + '":{')
 
     # rowDataArr.append(tableRow)
