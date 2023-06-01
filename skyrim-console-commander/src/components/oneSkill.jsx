4ec464d67375e7681ffe90128f8177c97b90ec45
donwung <113479299+donwung@users.noreply.github.com>
2023-05-31 17:13:33 -0700

    can print multilevel skills

--- skyrim-console-commander/src/components/oneSkill.jsx
+++ skyrim-console-commander/src/components/oneSkill.jsx
@@ -4,12 +4,12 @@ const OneSkill = (props) => {
     const skill = props.skill
     const handleOnCheckedSkill = props.handleOnCheckedSkill
     const handleOnSkillChange = props.handleOnSkillChange
-
+    const [showPerks, setShowPerks] = useState(false)
     // console.log(skill.perks)
 
     const [allPerks, setAllPerks] = useState(skill.perks)
 
-    console.log(allPerks)
+    // console.log(allPerks)
 
     return (
         <div>
@@ -27,22 +27,57 @@ const OneSkill = (props) => {
                     value={skill.selected ? skill.level : 0}
                 ></input>
             </div>
-            <div>
-                My perks here:
+            <p onClick={() => setShowPerks(!showPerks)}>
+                Show Perks
+            </p>
+            <div style={showPerks ? { backgroundColor: "green" } : { display: "none" }}>
                 {/* {showPerks(skill.perks)} */}
                 {/* BUG: cannot print out multilevel perks */}
                 {/* TODO: style buttons to look "normal" */}
                 {/* TODO: make buttons add to skyrim console output */}
                 {/* TODO: move perks to another component */}
                 {Object.keys(allPerks).map((onePerk) => {
+                    const rankedPerksArr = []
+                    // console.log(onePerk)
+                    if ("rank" in allPerks[onePerk]) {
+                        // console.log("haggling perk")
+                        console.log(allPerks[onePerk])
+                        for (let key in allPerks[onePerk].rank) {
+                            rankedPerksArr.push(allPerks[onePerk].rank[key])
+                            // console.log(key)
+                            console.log(allPerks[onePerk].rank[key].description)
+                        }
+                    }
+
+
                     return (
                         <div>
                             <p style={{ textAlign: "left" }}>
-                                {onePerk}: {allPerks[onePerk].description}
+
+                                <h2>
+                                    {onePerk}:
+                                </h2>
+                                <div style={{ border: "2px solid white" }}>
+
+                                </div>
+                                {allPerks[onePerk].description && <p>{allPerks[onePerk].description}</p>}
                                 <br />
-                                <button type="button" onClick={() => { console.log(allPerks[onePerk].ID) }}>
-                                    ID: {allPerks[onePerk].ID}
-                                </button>
+                                {rankedPerksArr.map((val) => {
+                                    return (
+                                        <div>
+                                            <p>{val.description}</p>
+                                            <button type="button" onClick={() => { console.log(val.ID) }}>
+                                                ID: {val.ID}
+                                            </button>
+                                        </div>
+                                    )
+                                })}
+                                {
+                                    allPerks[onePerk].ID &&
+                                    <button type="button" onClick={() => { console.log(allPerks[onePerk].ID) }}>
+                                        ID: {allPerks[onePerk].ID}
+                                    </button>
+                                }
                             </p>
                         </div>
                     )
