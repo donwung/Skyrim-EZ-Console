846ca2d86e99a1f2d261524740d1c7d811329ea5
donwung <113479299+donwung@users.noreply.github.com>
2023-07-13 15:17:38 -0700

    added a "how to"

--- Skyrim-EZ-Console/src/components/help.jsx
+++ Skyrim-EZ-Console/src/components/help.jsx
@@ -5,26 +5,53 @@ const Help = () => {
     const [isVisible, setIsVisible] = useState(false)
 
     return (
-        <div style={{ position: "absolute" }}>
+        <div style={{ position: "absolute", textAlign: "left", height: "0px", margin: "10px", zIndex: "99" }}>
             <p
-                onClick={() => { setIsVisible(!isVisible); console.log(isVisible) }}
+                onClick={() => { setIsVisible(!isVisible) }}
+                className="clickable unselectable"
             >
-                help
+                How To Use:
             </p>
-            <div style={isVisible ? { visibility: "visible", textAlign: "left", backgroundColor: "black", zIndex: "4" } : { visibility: "hidden", height: "0px", width: "0px" }}>
+            <div
+                style={isVisible ? { visibility: "visible", textAlign: "left", backgroundColor: "black", padding: "5px" } : { visibility: "hidden", height: "0px", width: "0px" }}
+            >
                 <p>
                     This is how to use SkyrimEZConsole:
                 </p>
                 <p>
-                    First, install ConsolePlusPlus using your favorite mod loader.
+                    First, install <a target="_blank" href="https://www.nexusmods.com/skyrimspecialedition/mods/79975">ConsolePlusPlus</a> for Skyrim Special Edition using your favorite mod loader.
                     <br></br>
                     Then open this application (at https://donwung.github.io/Skyrim-EZ-Console/)
+                </p>
+                <p>
+                    To set your skills, click on the skill name to enable changes.
                     <br></br>
-                    Set your stats
+                    Then drag the slider as desired.
+                </p>
+                <p>
+                    To set your perks, mouse over the "View Perks" text to open the perks selection menu.
                     <br></br>
-                    Copy the commands
+                    If the perk has a slider, you can select your desired rank.
                     <br></br>
-                    Then paste them into ConsolePlusPlus using Ctrl+V
+                    Then you can click the "+" button to add the perk to your list of console commands.
+                </p>
+                <p>
+                    When finished, you can mouse over the bottom part of the window.
+                    <br></br>
+                    Then click the "console" to enlarge it.
+                    <br></br>
+                    Here, you can see your skills and perks selected.
+                    <br></br>
+                    To remove a console command, click the "(remove)" button.
+                </p>
+                <p>
+                    You can then click "Copy to Clipboard" to copy all the commands to your clipboard.
+                    <br></br>
+                    Then go back into your game, open the Skyrim console using "`" (the key under Esc),
+                    <br></br>
+                    then paste your command using Ctrl+V.
+                    <br></br>
+                    If done correctly, you should have your updated stats and perks!
                 </p>
             </div>
         </div>
